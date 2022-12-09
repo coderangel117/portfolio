afaa1648c25d0daefc43f489c319b96e78d0313a
Gabriel PERINO <perino_g@etna-alternance.net>
2022-12-09 17:26:21 +0100

    refactor[mail] : contact.php  - Add reply-to header in order to get mail user

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
 <?php
 $retour = mail('perino.contact@gmail.com',
-    'Formulaire de contact rempli ',
+    'Formulaire de contact rempli',
     $_POST['message'],
-    "from:webmaster@gabrielperino.com");
+    "from:webmaster@gabrielperino.com" ."\r\n" . $_POST['mail']);
 if ($retour){
     echo '<p>Votre message a bien été envoyé.</p>';
     header('index.html');
