5f36138545c77831f6a00bc315d39597a58e6cfc
Gabriel PERINO <perino_g@etna-alternance.net>
2022-12-16 11:25:01 +0100

    fix[contact] : redirection when form is empty - Add conditionif$_post is empty => redirect to /

--- contact.php
+++ contact.php
@@ -1,9 +1,14 @@
 <?php
-$retour = mail('perino.contact@gmail.com',
+if (!empty($_POST)){
+    $retour = mail('perino.contact@gmail.com',
     'Formulaire de contact rempli',
     $_POST['message'],
     "from:webmaster@gabrielperino.com" ."\r\n" . $_POST['mail']);
-if ($retour){
-    echo '<p>Votre message a bien été envoyé.</p>';
-    header('index.html');
+    if ($retour){
+        header("Location:/");?>
+        <script>alert("Votre message a bien été envoyé, je vous remercie")</script>
+        <?php
+    }
 }
+header("Location:/");?>
+<a href="https://gabrielperino.com/">retourner sur la page d'accueil</a>
