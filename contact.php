b073cf5fb2a10544e65cf83f96b933f6b56d533b
Gabriel PERINO <perino_g@etna-alternance.net>
2022-12-17 01:24:59 +0100

    refactor[mail] : - remove script alert - update message mail - Add redirection to index

--- contact.php
+++ contact.php
@@ -2,13 +2,16 @@
 if (!empty($_POST)){
     $retour = mail('perino.contact@gmail.com',
     'Formulaire de contact rempli',
-    $_POST['message'],
+    "L'utilisateur :" . $_POST['mail'] . "\r\n vous a contacté au sujet de : " . $_POST['subject'] ."\r\n Voici son message : ". $_POST['message'],
     "from:webmaster@gabrielperino.com" ."\r\n" . $_POST['mail']);
     if ($retour){
-        header("Location:/");?>
-        <script>alert("Votre message a bien été envoyé, je vous remercie")</script>
-        <?php
+        header("Location:/");
+    }
+    else{
+    header("Location:/");
     }
 }
 header("Location:/");?>
+        <script>alert("Votre message a bien été envoyé, je vous remercie")</script>
+
 <a href="https://gabrielperino.com/">retourner sur la page d'accueil</a>
