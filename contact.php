6b1e87270978ea9665829567285f78963e77c0d5
gab <perinogabriel1@gmail.com>
2023-09-15 22:06:20 +0200

    feat[mail] : contact.php - Set secret in .env and use variable in url

--- contact.php
+++ contact.php
@@ -2,70 +2,71 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 // On vérifie si le champ "recaptcha-response" contient une valeur
-    if (empty($_POST['recaptcha-response'])) {
-        header('Location: index.html');
+    if (!empty($_POST['recaptcha-response'])) {
+        $envFilePath = __DIR__ . '/.env';
 
-    } else {
-        // On prépare l'URL
-        $url = "https://www.google.com/recaptcha/api/siteverify?secret=6LdZZCooAAAAAALpkddMatKj84lyv091TcfEEj87&response={$_POST['recaptcha-response']}";
+        if (file_exists($envFilePath)) {
+            $envContent = file_get_contents($envFilePath);
+            $envVariables = parse_ini_string($envContent);
+            $secret = $envVariables['SECRET'];
 
-        // On vérifie si curl est installé
-        if (function_exists('curl_version')) {
-            $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_HEADER, false);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_TIMEOUT, 1);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            $response = curl_exec($curl);
-        } else {
-            // On utilisera file_get_contents
-            $response = file_get_contents($url);
-        }
-        // On vérifie qu'on a une réponse
-        if (empty($response)) {
-            echo "no response ";
-        } else {
-            $data = json_decode($response);
-            if ($data->success) {
-                if (
-                    !empty($_POST['nom']) &&
-                    !empty($_POST['sujet']) &&
-                    !empty($_POST['email']) &&
-                    !empty($_POST['message'])
-                ) {
-                    // On nettoie le contenu
-                    $nom = strip_tags($_POST['nom']);
-                    $sujet = strip_tags($_POST['sujet']);
-                    $email = strip_tags($_POST['email']);
-                    $message = htmlspecialchars($_POST['message']);
+            $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secret&response={$_POST['recaptcha-response']}";
 
-
-                    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-                        $regex = '/^[a-zA-Z0-9+_.-]+@gabrielperino.com+$/';
-                        $match = preg_match($regex, $_POST['email']);
-                        if (!$match) {
-                            $retour = mail('perino.contact@gmail.com',
-                                'Formulaire de contact rempli',
-                                "L'utilisateur :" . $_POST['email'] . "\r\n vous a contacté au sujet de : " . $_POST['sujet'] . "\r\n Voici son message : " . $_POST['message'],
-                                "from:webmaster@gabrielperino.com" . "\r\n" . $_POST['email']);
-                            header('Location: index.html');
-
+            if (function_exists('curl_version')) {
+                $curl = curl_init($url);
+                curl_setopt($curl, CURLOPT_HEADER, false);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                $response = curl_exec($curl);
+            } else {
+                // On utilisera file_get_contents
+                $response = file_get_contents($url);
+            }
+            // On vérifie qu'on a une réponse
+            if (!empty($response)) {
+                $data = json_decode($response);
+                if ($data->success) {
+                    if (
+                        !empty($_POST['nom']) &&
+                        !empty($_POST['sujet']) &&
+                        !empty($_POST['email']) &&
+                        !empty($_POST['message'])
+                    ) {
+                        // On nettoie le contenu
+                        $nom = strip_tags($_POST['nom']);
+                        $sujet = strip_tags($_POST['sujet']);
+                        $email = strip_tags($_POST['email']);
+                        $message = htmlspecialchars($_POST['message']);
+                        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                            $regex = '/^[a-zA-Z0-9+_.-]+@gabrielperino.com+$/';
+                            $match = preg_match($regex, $_POST['email']);
+                            if (!$match) {
+                                $retour = mail('perino.contact@gmail.com',
+                                    'Formulaire de contact rempli',
+                                    "L'utilisateur :" . $_POST['email'] . "\r\n vous a contacté au sujet de : " . $_POST['sujet'] . "\r\n Voici son message : " . $_POST['message'],
+                                    "from:webmaster@gabrielperino.com" . "\r\n" . $_POST['email']);
+                                header('Location: /');
+                            } else {
+                                header('Location: index.html');
+                            }
                         } else {
                             header('Location: index.html');
-
                         }
-
                     } else {
                         header('Location: index.html');
                     }
-
                 } else {
                     header('Location: index.html');
                 }
             } else {
                 header('Location: index.html');
             }
+        } else {
+            header('Location: index.html');
         }
+    } else {
+        header('Location: index.html');
     }
 } else {
     http_response_code(405);
