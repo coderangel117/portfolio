69aebab6e51104885b73ec4046f2f7a8a9b068d5
gab <perinogabriel@gmail.com>
2024-09-19 16:44:42 +0200

    refactor [navbar] : add theme + refactor navbar to vertical navbar

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+
+const ThemeButton = document.getElementById("ThemeButton");
+const html = document.querySelector("html");
+
+
+let DOM_img = document.createElement("img");
+DOM_img.src = "images/icon-cross.svg";
+
+// START ADD DELETE BUTTON
+let dBtn = document.createElement("button");
+dBtn.classList.add("button");
+dBtn.appendChild(DOM_img);
+function ToggleTheme() {
+    html.classList.toggle("theme-dark");
+    html.classList.toggle("theme-light");
+}
+
+function ToggleImageTheme() {
+    const sun_img = document.createElement("img");
+    const moon_img = document.createElement("img");
+    moon_img.src = "images/icon-moon.svg";
+    sun_img.src = "images/icon-sun.svg";
+
+    ThemeButton.classList.toggle("dark");
+    ThemeButton.classList.toggle("light");
+    if (ThemeButton.classList.contains("light")) {
+        ThemeButton.appendChild(moon_img);
+    } else {
+        ThemeButton.appendChild(sun_img);
+    }
+    ThemeButton.removeChild(ThemeButton.children[0]);
+}
+
+ThemeButton.addEventListener("click", function () {
+    ToggleImageTheme();
+    ToggleTheme();
+})
+
 function enlargeImage(image) {
     let enlargedImage = document.createElement('div');
     enlargedImage.className = 'enlarged';
