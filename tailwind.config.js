7b46c7ee1cbc1faebc5417bfc5ba56801711212f
Gabriel PERINO <perinogabriel1@gmail.com>
2025-11-27 22:03:20 +0100

    feat: Add font - Inter & Jetbrains-Mono - Fix layout on responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,9 +81,8 @@ export default {
                 },
             },
             fontFamily: {
-                sans: ["var(--font-sans)"],
-                serif: ["var(--font-serif)"],
-                mono: ["var(--font-mono)"],
+                Inter: ["Inter", "sans-serif"],
+                Jetbrains: ["Jetbrains", "sans-serif"],
             },
             keyframes: {
                 "accordion-down": {
